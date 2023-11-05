51446d6f51f72e9e196e546c00b814ff63c24f89
p7uverma <priyanshu.creator.dev@gmail.com>
2023-11-05 20:13:16 +0530

    Fix missing throwIfMissing call and update push notification logic.

--- src/main.ts
+++ src/main.ts
@@ -8,36 +8,34 @@ type Context = {
 };
 
 export default async ({ req, res, log, error }: Context) => {
-  throwIfMissing(process.env, [
-    'FCM_PROJECT_ID',
-    'FCM_PRIVATE_KEY',
-    'FCM_CLIENT_EMAIL',
-    'FCM_DATABASE_URL',
-  ]);
-
   try {
+    throwIfMissing(process.env, [
+      'FCM_PROJECT_ID',
+      'FCM_PRIVATE_KEY',
+      'FCM_CLIENT_EMAIL',
+      'FCM_DATABASE_URL',
+    ]);
     throwIfMissing(req.body, ['deviceToken', 'message']);
     throwIfMissing(req.body.message, ['title', 'body']);
-
-    log(`Sending message to device: ${req.body.message}`);
-
-    try {
-      const response = await sendPushNotification({
-        notification: {
-          title: req.body.message.title,
-          body: req.body.message.body,
-        },
-        token: req.deviceToken,
-      });
-      log(`Successfully sent message: ${response}`);
-
-      return res.json({ ok: true, messageId: response });
-    } catch (e) {
-      error(e);
-      return res.json({ ok: false, error: 'Failed to send the message' }, 500);
-    }
   } catch (err) {
     error(err);
     if (err instanceof Error) res.json({ ok: false, error: err.message }, 400);
+  }
+
+  try {
+    log(`Sending message to device: ${req.body}`);
+    const response = await sendPushNotification({
+      notification: {
+        title: req.body.message.title,
+        body: req.body.message.body,
+      },
+      token: req.deviceToken,
+    });
+    log(`Successfully sent message: ${response}`);
+
+    return res.json({ ok: true, messageId: response });
+  } catch (e) {
+    error(e);
+    return res.json({ ok: false, error: 'Failed to send the message' }, 500);
   }
 };
