b1a2b19e80518439ff7e0151076e1fd3e88969e0
p7uverma <priyanshu.creator.dev@gmail.com>
2023-11-05 20:10:19 +0530

    catch at last

--- src/main.ts
+++ src/main.ts
@@ -18,26 +18,26 @@ export default async ({ req, res, log, error }: Context) => {
   try {
     throwIfMissing(req.body, ['deviceToken', 'message']);
     throwIfMissing(req.body.message, ['title', 'body']);
+
+    log(`Sending message to device: ${req.body.message}`);
+
+    try {
+      const response = await sendPushNotification({
+        notification: {
+          title: req.body.message.title,
+          body: req.body.message.body,
+        },
+        token: req.deviceToken,
+      });
+      log(`Successfully sent message: ${response}`);
+
+      return res.json({ ok: true, messageId: response });
+    } catch (e) {
+      error(e);
+      return res.json({ ok: false, error: 'Failed to send the message' }, 500);
+    }
   } catch (err) {
     error(err);
     if (err instanceof Error) res.json({ ok: false, error: err.message }, 400);
-  }
-
-  log(`Sending message to device: ${req.body.message}`);
-
-  try {
-    const response = await sendPushNotification({
-      notification: {
-        title: req.body.message.title,
-        body: req.body.message.body,
-      },
-      token: req.deviceToken,
-    });
-    log(`Successfully sent message: ${response}`);
-
-    return res.json({ ok: true, messageId: response });
-  } catch (e) {
-    error(e);
-    return res.json({ ok: false, error: 'Failed to send the message' }, 500);
   }
 };
